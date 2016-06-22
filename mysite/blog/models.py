c5067cc905e3c0db66c93f76b9109bba429fd879
chentianma <chentianma@github.com>
2016-06-22 12:41:00 +0800

    add view function and home.html

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,40 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+# Tag model
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.tag_name
+
+
+# Category model
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category_name
+
+
+# Article model
 class Article(models.Model):
-	
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    publish_date = models.DateTimeField(blank=True, null=True)
+    tags = models.ManyToManyField(Tag)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
