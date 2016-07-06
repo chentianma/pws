6b61279a250efd9aae5b21c084df87d7ab326cac
chentianma <819252022@qq.com>
2016-07-07 00:06:20 +0800

    新建templates下的blog模板文件夹

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from .views import home
+from .views import blogHome
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls'))
+    url(r'^$', 'blog.views.blogHome'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def blogHome(request):
+    return render(request, 'blog/bloghome.html')
