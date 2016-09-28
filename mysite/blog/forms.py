8f74c24eaf58065b16ae4635874adf91ac65d661
chentianma <chentianma@github.com>
2016-09-28 16:30:24 +0800

    导航栏的颜色由黑改为白；优化了postForm的代码，自定义样式；用js优化了login页面的表单样式（！-！）

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Article
+from django.contrib.auth.models import User
 
 
 class postForm(forms.ModelForm):
@@ -7,3 +8,24 @@ class postForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = ('title', 'description', 'text', 'category', 'tags')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title', }),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'tags': forms.SelectMultiple(attrs={'class': 'form-control'}),
+        }
+        # title = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title', }))
+        # description = forms.Textarea()
+        # text = forms.Textarea()
+        # category = forms.Select()
+        # tags = forms.SelectMultiple()
+
+
+# class NewUserForm(forms.ModelForm):
+#
+#     class Meta:
+#         model = User
+#         fields = ('username', 'password')
+#         widgets = {
+#             'username': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Username', }),
+#             'password': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password', })
+#         }
